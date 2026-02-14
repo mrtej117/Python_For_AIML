cdd029e878202e20a903c06cbb650493e581cb01
mrtej117 <mrtej117@gmail.com>
2026-02-14 13:34:58 +0530

    Add marks calculator for five subjects

---
+++ basic_problem_solving/marks_calculator.py
@@ -0,0 +1,8 @@
+subject1 = float(input("enter marks "))
+subject2 = float(input("enter marks "))
+subject3 = float(input("enter marks "))
+subject4 = float(input("enter marks "))
+subject5 = float(input("enter marks "))
+total_marks = subject1 + subject2 + subject3 + subject4 + subject5
+average_marks = total_marks/5
+percentage = (total_marks / 500 * 100)
