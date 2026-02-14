26301bd5a5c473ff577ca69605c5f274846fbd8b
mrtej117 <mrtej117@gmail.com>
2026-02-14 14:54:30 +0530

    Add print statements for total, average, and percentage

--- basic_problem_solving/marks_calculator.py
+++ basic_problem_solving/marks_calculator.py
@@ -6,3 +6,6 @@ subject5 = float(input("enter marks "))
 total_marks = subject1 + subject2 + subject3 + subject4 + subject5
 average_marks = total_marks/5
 percentage = (total_marks / 500 * 100)
+print("Total Marks:", total_marks)
+print("Average Marks:", average_marks)
+print("Percentage:", percentage)
