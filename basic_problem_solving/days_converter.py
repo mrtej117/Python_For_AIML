4a83aad1352a56ac9710d65da6201d19b510a432
mrtej117 <mrtej117@gmail.com>
2026-02-14 13:27:49 +0530

    Add days converter for years, months, and weeks

---
+++ basic_problem_solving/days_converter.py
@@ -0,0 +1,8 @@
+days = int(input("enter number of days :"))
+years = days / 365
+months = days / 30
+weeks = days / 7
+print("days to year",years)
+print("days to months",months)
+print("days to weeks",weeks)
+
