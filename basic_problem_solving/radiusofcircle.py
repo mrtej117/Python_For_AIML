354513c5fc66d7ff6f3436492d3d0c7a8c1a645e
mrtej117 <mrtej117@gmail.com>
2026-02-16 21:53:34 +0530

    Create radiusofcircle.py for area calculation
    Add a Python script to calculate the area of a circle using both math module and a constant.

---
+++ basic_problem_solving/radiusofcircle.py
@@ -0,0 +1,15 @@
+
+############################################################## with math
+import math
+
+radius= int(input("enter value lenght  in : "))
+
+radius_of_circle=  math.pi *radius**2
+print(radius_of_circle)
+
+############################################################## normal
+radius= int(input("enter value lenght  in : "))
+
+radius_of_circle = 3.14*radius**2
+
+print(radius_of_circle)
