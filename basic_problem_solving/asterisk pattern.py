eb0c7d00d9cddc7a1795721f59dbf6c11673ee30
mrtej117 <mrtej117@gmail.com>
2026-02-16 22:08:13 +0530

    Implement asterisk pattern with while and for loops
    Added asterisks pattern printing using both while and for loops.

---
+++ basic_problem_solving/asterisk pattern.py
@@ -0,0 +1,11 @@
+#by while loop
+i = 1
+rows = 9
+while i < rows:
+    print("* "*i)
+    i = i + 2
+####################################################
+#BY FOR LOOP
+
+for i in range(1,9,2):
+    print("* "*i)
