554bcad08a8c6cfd8eaa246f1df7c07b5737ac2f
mrtej117 <mrtej117@gmail.com>
2026-02-17 18:59:34 +0530

    Update days_converter.py

--- basic_problem_solving/days_converter.py
+++ basic_problem_solving/days_converter.py
@@ -1,8 +1,11 @@
-days = int(input("enter number of days :"))
-years = days / 365
-months = days / 30
-weeks = days / 7
-print("days to year",years)
-print("days to months",months)
-print("days to weeks",weeks)
+days = int(input("enter days : "))
+years = days//365
+remaing_days =days-years*365
+months = remaing_days // 30
+remaing_days =remaing_days-months*30
+weeks = remaing_days // 7
+remaining_days = days-weeks*7
 
+daysu =remaing_days
+
+print(years,"years",months,"months",weeks,"weeks ",daysu,"daysu")
