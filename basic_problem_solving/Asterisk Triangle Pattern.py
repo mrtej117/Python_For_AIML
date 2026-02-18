7de70f49e9256f35be7b5f3ad1c4bef7dc87a17c
mrtej117 <mrtej117@gmail.com>
2026-02-18 05:44:42 +0530

    Add asterisk triangle pattern implementation

---
+++ basic_problem_solving/Asterisk Triangle Pattern.py
@@ -0,0 +1,21 @@
+row = 4
+for i in range(1, rows + 1):
+    for j in range(2 * i - 1):
+        print("*", end=" ")
+    print()
+
+############################################################
+
+
+rows = int(input("enter a num"))
+for i in range(1,rows+1):
+
+    for j in range(i):
+        print("*",end= " ")
+    print( )
+
+
+####################################################
+
+a = 4
+while()
