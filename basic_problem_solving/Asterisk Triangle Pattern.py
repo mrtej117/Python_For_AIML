30279038c81ceecae86e19349f6e94031a850c01
mrtej117 <mrtej117@gmail.com>
2026-02-18 06:25:27 +0530

    Refactor Asterisk Triangle Pattern for user input
    Removed hardcoded rows value and added user input for rows.

--- basic_problem_solving/Asterisk Triangle Pattern.py
+++ basic_problem_solving/Asterisk Triangle Pattern.py
@@ -17,5 +17,30 @@ for i in range(1,rows+1):
 
 ####################################################
 
-a = 4
-while()
+i = 1
+rows = 8 
+while(i<=rows):
+    j =1
+    while j<= (2*i-1):
+        print("*",end=" ")
+        j = j+1
+    print()
+    i = i+1
+
+
+
+i = 1
+rows = int(input("enter a num")) 
+while(i<=rows):
+    j =1
+    while j<= (2*i-1):
+        print("*",end=" ")
+        j = j+1
+    print()
+    i = i+1
+
+
+
+
+
+
