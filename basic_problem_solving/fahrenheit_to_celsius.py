94dcdb2f4b40f08a60847045f34a3328a19c16d5
MR.TEJ117 <mrtej117@gmail.com>
2026-02-21 07:11:59 +0530

    Add function to convert Fahrenheit to Celsius
    Refactor temperature conversion logic into a function.

---
+++ basic_problem_solving/fahrenheit_to_celsius.py
@@ -0,0 +1,14 @@
+f = float(input ("enter fahrenheit : "))
+c = 5/9*(f-32)
+print(f,"F= ",c," celsius degree",)
+#################################################################################
+
+def c_f(f):
+    c = 5/9*(f-32)
+    return(c)
+celsius =c_f(20)
+print(celsius )
+
+
+
+
