a02a666e7221d8c03b9499c150ab93ef2b46786b
MR.TEJ117 <mrtej117@gmail.com>
2026-02-27 13:57:48 +0530

    Update days_converter.py

--- basic_problem_solving/days_converter.py
+++ basic_problem_solving/days_converter.py
@@ -4,7 +4,7 @@ remaing_days =days-years*365
 months = remaing_days // 30
 remaing_days =remaing_days-months*30
 weeks = remaing_days // 7
-remaining_days = days-weeks*7
+remaining_days = remaing_days-weeks*7
 
 daysu =remaing_days
 
